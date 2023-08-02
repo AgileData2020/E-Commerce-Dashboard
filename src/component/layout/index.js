fc7ad3551497bd0dd92347c4714e0a230cc59034
afzalakram007 <afzal.akram@mltitans.com>
2023-08-02 17:14:28 +0500

    dashboard and upload screen complete

--- src/component/layout/index.js
+++ src/component/layout/index.js
@@ -7,7 +7,7 @@ const BottomBar = ({ children }) => {
     const [value, setValue] = React.useState(0);
     return (
         <>
-            <h1>Layout like sidebar bottom navigation</h1>
+          
             {children}
         </>
     )
--- src/upload/index.js
+++ src/upload/index.js
@@ -1,8 +1,9 @@
-
-
 import React, { useState } from 'react'
-
+import './style.css'
 import { useNavigate } from 'react-router-dom';
+import { Badge, Button } from 'rsuite';
+import { Uploader } from 'rsuite';
+import { Progress } from 'rsuite';
 
 
 
@@ -11,8 +12,48 @@ function Upload() {
   const navigate = useNavigate();
   return (
     <>
-      <h1>Upload</h1>
+ <header className='header-bg'>
 
+
+<div className='togal-button'>
+    {/* <NavToggle expand={expand} onChange={() => setExpand(!expand)} /> */}
+</div>
+
+<div className='togal-right pd-13'>
+    <div className='flot-left ms-30'>
+        <Badge>
+            <Button >
+                <img src="notiicatin.png" /></Button>
+        </Badge>
+    </div>
+    <div className='flot-left'>
+        <img className='profile-pic' src="profile-img.jpg" alt="logo" />
+    </div>
+    <div className='flot-left profile-detail'> Hello, Andrew!</div>
+
+</div>
+</header>
+<main>
+  <div className='upload-data'>
+<Uploader className='upload-area'  action="//jsonplaceholder.typicode.com/posts/" draggable>
+      <div style={{  alignItems: 'center', justifyContent: 'center'  }}>
+      <img className='' src="uload-icon.png" alt="logo" />
+        <div className='upload-text'>Upload Data File</div>
+        <span>or <span className='blue-text'>browse</span>  your file to upload</span>
+        <p className='upload-para'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+      </div>
+    </Uploader>
+    <div className='progress'>
+  
+    <Progress.Line percent={100} status="success" />
+
+    </div>
+    </div>
+<div className='text-center'  style={{  alignItems: 'center', justifyContent: 'center'  }}>
+<Button appearance="primary " className='btn-side'>Done</Button>
+</div >
+   
+    </main>
     </>
   )
 }
