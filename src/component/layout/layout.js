74a462de0b5f5392f589f95465010d84a4425915
afzalakram007 <afzal.akram@mltitans.com>
2023-08-17 12:51:43 +0500

    layout change

--- src/component/layout/layout.js
+++ src/component/layout/layout.js
@@ -5,11 +5,12 @@ import AngleLeftIcon from '@rsuite/icons/legacy/AngleLeft';
 import AngleRightIcon from '@rsuite/icons/legacy/AngleRight';
 import hydrocarbonIcon from '../../assets/img/hydloogo.png';
 import hydrocarbonIconMobile from '../../assets/img/hydro-mobile.png'
-import PlusIcon from '@rsuite/icons/Plus';
+
 import { Badge, Button } from 'rsuite';
 import { Nav, Tab } from 'rsuite';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import CustomeDrawer from '../customeDrawer';
 const headerStyles = {
     padding: 2,
     fontSize: 16,
@@ -37,7 +38,7 @@ const NavToggle = ({ expand, onChange }) => {
 
 
 const Layout = ({ children }) => {
-    const [expand, setExpand] = useState(true);
+    const [expand, setExpand] = useState(false);
     const navigate = useNavigate();
     const logoutUser = () => {
         localStorage.clear();
@@ -47,123 +48,24 @@ const Layout = ({ children }) => {
 
     const userInfo = useSelector(state => state.login);
 
-    console.log(userInfo?.first_name)
+
     return (
         <div className="show-fake-browser sidebar-page">
             <div className='top-line'></div>
+          
+
             <Container>
-                <Sidebar className='sidenav-bar' width={expand ? 260 : 56} collapsible              >
-                    <div className=' side-fixed'>
-                        <Sidenav.Header>
-                            <div style={headerStyles}>
-
-                                <img src={!expand ? hydrocarbonIconMobile : hydrocarbonIcon} alt="logo" />
-
-
-                            </div>
-                        </Sidenav.Header>
-                        <Sidenav expanded={expand} defaultOpenKeys={['3']} appearance="subtle">
-                            <Sidenav.Body>
-                                <Nav>
-
-
-                                    <Nav.Menu
-                                        eventKey="1"
-                                        trigger="hover"
-                                        title="Jan 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-                                    <Nav.Menu
-                                        eventKey="2"
-                                        trigger="hover"
-                                        title="Fab 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-
-
-                                    <Nav.Menu
-                                        eventKey="4"
-                                        trigger="hover"
-                                        title="Mar 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-                                    <Nav.Menu
-                                        eventKey="5"
-                                        trigger="hover"
-                                        title="Apr 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-                                    <Nav.Menu
-                                        eventKey="6"
-                                        trigger="hover"
-                                        title="May 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-                                    <Nav.Menu
-                                        eventKey="7"
-                                        trigger="hover"
-                                        title="Jan 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-                                    <Nav.Menu
-                                        eventKey="8"
-                                        trigger="hover"
-                                        title="July 2023"
-                                        icon={<PlusIcon />}
-                                        placement="rightStart"
-                                    >
-                                        <Nav.Item eventKey="3-1">Balance</Nav.Item>
-                                        <Nav.Item eventKey="3-2">Model</Nav.Item>
-
-                                    </Nav.Menu>
-
-                                </Nav>
-                            </Sidenav.Body>
-                        </Sidenav>
-                    </div>
-                </Sidebar>
-
+                
+                <CustomeDrawer open={expand} setOpen={setExpand}/>
                 <Container>
                     <header className='header-bg'>
 
 
-                        <div className='togal-button'>
-                            <NavToggle expand={expand} onChange={() => setExpand(!expand)} />
+                        <div className=''>
+                      <div className='flot-left mob-logo'><img src={hydrocarbonIconMobile} alt="logo" /></div>  
+                      <div className='flot-left'> <NavToggle expand={expand} onChange={() => setExpand(!expand)} /></div>
+                         
+                         
                         </div>
 
                         <div className='togal-right pd-13'>
@@ -188,12 +90,15 @@ const Layout = ({ children }) => {
 
                         </div>
                     </header>
-                    <main>
+                    <Content className='content'>
+                {children}
 
-                        {children}
-                    </main>
+</Content>
                 </Container>
-            </Container>
+                </Container>
+               
+
+           
         </div>
     );
 }
