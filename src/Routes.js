284d85a49b6d8579ec277b71fc69a66d8f5f0044
Amir <amir.yousaf@odysseyanalytics.net>
2023-10-03 22:44:49 +0500

    Updates

--- src/Routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    routesPath: {
-        Login: '/',
-        Dashboard: '/dashboard',
-        Revenue_analysis: '/revenue/analysis',
-        Inventory: '/inventory'
-    }
-}
--- src/api/endPoints.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    auth: {
-        Login: '/login',
-        Register: '/register'
-    },
-
-}
